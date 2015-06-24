a7b2243307cdf638e0fb5b400dc051cc4b8a3190
Colin Pringle-Wood <colin.pringlewood@gmail.com>
2015-06-24 09:44:42 +0100

    making instructions clear

--- client.py
+++ client.py
@@ -11,7 +11,7 @@ s = socket(AF_INET, SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 # s.send(MESSAGE)
 while True:
-    input = raw_input('enter a interger or a list of intergers seperated by a comma: ')
+    input = raw_input('enter a interger or a list of intergers seperated by a comma. eg: 2, 6: ')
     s.send(input)
     data = s.recv(BUFFER_SIZE)
     if data: print data
